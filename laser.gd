168b72f6f1f6ad79e037c1bdd07cc5b4938cfae9
Gabriel447 <fullstackgabe@gmaill.com>
2025-02-01 14:41:33 -0300

    fire ok

---
+++ laser.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var speed: float = 600
+
+func _process(delta):
+	position.y -= speed * delta
+	
+func _on_area_entered(area):
+	if area.is_in_group("enemy"):
+		self.queue_free()
